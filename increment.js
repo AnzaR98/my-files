030c7bba0f588651eb59ed5482e7eaf3bb19d89f
AnzaR98 <107769609+AnzaR98@users.noreply.github.com>
2026-04-15 04:04:29 -0700

    Add files via upload
    Signed-off-by: AnzaR98 <107769609+AnzaR98@users.noreply.github.com>

---
+++ increment.js
@@ -0,0 +1,13 @@
+//initialize count as 0
+//check for clicks on the increment button
+//increment the count variable when the button is clicked
+//change the count-el in the HTML to reflect the new count
+
+let countEl = document.getElementById("count-el");
+let count = 0;
+
+function increment() {
+    count += 1;
+    countEl.innerText = count;
+}
+ 
